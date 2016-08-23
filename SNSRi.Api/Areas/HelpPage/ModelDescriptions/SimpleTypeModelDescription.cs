76c29e7cfdade6b06c75449c67d1ce563cf14ce4
Jojo Aquino <jfaquinojr@gmail.com>
2016-08-23 23:33:50 +0800

    Add project files.

---
+++ SNSRi.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SNSRi.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ SNSRi.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SNSRi.Api.WebApiApplication" Language="C#" %>
