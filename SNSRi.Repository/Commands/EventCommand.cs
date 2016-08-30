02355e7b799ccb39369804ff91623b92c385bdfe
Jojo Aquino <jfaquinojr@gmail.com>
2016-08-31 02:49:51 +0800

    update Device.Status upon ChangeValueEvent

--- SNSRi.Repository/Commands/EventCommand.cs
+++ SNSRi.Repository/Commands/EventCommand.cs
@@ -15,6 +15,11 @@ namespace SNSRi.Repository.Commands
             entity.CreatedOn = DateTime.Now;
             const string sql = "insert into Event(DeviceId, OccurredOn, NewStatus, OldStatus, Notes, CreatedOn, CreatedBy) values(@DeviceId, @OccurredOn, @NewStatus, @OldStatus, @Notes, @CreatedOn, @CreatedBy); SELECT last_insert_rowid()";
             var id = _connection.Query<int>(sql, entity).Single();
+
+            // update new status of device
+            string sqlUpdateDevice = $"update Device set Status = '{entity.NewStatus}' Where Id = {entity.DeviceId}";
+            _connection.Execute(sqlUpdateDevice);
+
             return id;
         }
 
