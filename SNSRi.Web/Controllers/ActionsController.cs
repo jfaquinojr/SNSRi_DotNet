823a84218a7fde12ccb9af3e2f08e44fe42e5715
Jojo Aquino <jfaquinojr@gmail.com>
2016-09-14 21:58:59 +0800

    Metro CSS

---
+++ SNSRi.Web/Controllers/ActionsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+using SNSRi.Repository.Query;
+
+namespace SNSRi.Web.Controllers
+{
+    public class ActionsController : ApiController
+    {
+		// GET: api/Actions
+        [HttpGet()]
+		[Route("api/Actions/GetOpenTickets/")]
+		public IHttpActionResult GetOpenTickets()
+		{
+		    var query = new TicketQuery();
+		    var result = query.GetOpenEventTickets();
+
+	        return Ok(result);
+        }
+
+		// GET: api/Actions/5
+		[Route("api/Actions/{id}")]
+		public IHttpActionResult Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST: api/Actions
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Actions/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Actions/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
