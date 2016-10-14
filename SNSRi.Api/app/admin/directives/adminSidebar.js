93dc78cbd708f5606c97d6bbcc3b9ecda27c0354
Jojo Aquino <jfaquinojr@gmail.com>
2016-10-14 17:47:41 +0800

    sidebar is now a directive

---
+++ SNSRi.Api/app/admin/directives/adminSidebar.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("app-admin");
+
+app.directive("adminSidebar", function() {
+  return {
+    restrict: "E",
+    templateUrl: "/Admin/AdminSidebar",
+    scope: true,
+    controller: function($scope) {
+      
+    }
+  }
+})
