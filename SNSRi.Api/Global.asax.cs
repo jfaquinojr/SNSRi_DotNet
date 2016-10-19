1c7ff7494e31a540df08d3e3f6e9fcd7841b6c1b
Jojo Aquino <jfaquinojr@gmail.com>
2016-10-19 20:51:07 +0800

    add json formatting to web api

--- SNSRi.Api/Global.asax.cs
+++ SNSRi.Api/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -23,6 +24,8 @@ namespace SNSRi.Api
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-		}
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("json", "true", "application/json"));
+        }
 	}
 }
