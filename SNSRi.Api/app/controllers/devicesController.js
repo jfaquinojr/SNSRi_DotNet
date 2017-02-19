3f81b4915cff805f72acc89ee4f9175b51247be9
jfaquinojr@gmail.com <jfaquinojr@gmail.com>
2017-02-19 10:43:48 +0800

    TileGroups

--- SNSRi.Api/app/controllers/devicesController.js
+++ SNSRi.Api/app/controllers/devicesController.js
@@ -3,6 +3,7 @@ app.controller("DevicesController", function ($scope, $http, $routeParams, dataS
     var scope = $scope;
     var svc = $http;
     var params = $routeParams;
+    scope.DeviceGroups = [];
     scope.Devices = [];
     scope.Room = {
         Id: 0,
@@ -13,6 +14,23 @@ app.controller("DevicesController", function ($scope, $http, $routeParams, dataS
             scope.Devices = _.reject(result.data, function (device) {
                 return device.HideFromView;
             });
+            //group by TileGroup:
+            //[{ TileGroup: "null", Devices: [] }, { TileGroup: "Section1", Devices: [] },]
+            scope.DeviceGroups = _
+                .chain(scope.Devices)
+                .groupBy('TileGroup')
+                .map(function (device, key) {
+                return {
+                    TileGroup: key,
+                    Devices: device
+                };
+            })
+                .value();
+            /*
+            for (let deviceGroup of groups) {
+                // further divide groups that result to 8 columns and above
+            }
+            */
         });
     };
     var loadRoom = function (roomId) {
@@ -23,5 +41,20 @@ app.controller("DevicesController", function ($scope, $http, $routeParams, dataS
     loadRoom(params.id);
     loadDevicesByRoomId(params.id);
     startScreenService.refreshStartScreen();
+    scope.sizeOfGroup = function (size) {
+        if (size > 24)
+            return "seven";
+        if (size > 20)
+            return "six";
+        if (size > 16)
+            return "five";
+        if (size > 12)
+            return "four";
+        if (size > 8)
+            return "three";
+        if (size > 4)
+            return "two";
+        return "";
+    };
 });
 //# sourceMappingURL=devicesController.js.map
--- SNSRi.Api/app/controllers/devicesController.ts
+++ SNSRi.Api/app/controllers/devicesController.ts
@@ -1,11 +1,12 @@
 ﻿var app = angular.module("app");
-
 app.controller("DevicesController",
     function ($scope, $http, $routeParams, dataService, startScreenService) {
         var scope = $scope;
         var svc = $http;
         var params = $routeParams;
 
+        scope.DeviceGroups = [];
+        
         scope.Devices = [];
         scope.Room = {
             Id: 0,
@@ -20,6 +21,25 @@ app.controller("DevicesController",
                     return device.HideFromView;
                 });
 
+                //group by TileGroup:
+                //[{ TileGroup: "null", Devices: [] }, { TileGroup: "Section1", Devices: [] },]
+                scope.DeviceGroups = _
+                    .chain(scope.Devices)
+                    .groupBy('TileGroup')
+                    .map(function (device: any, key: string) {
+                        return {
+                            TileGroup: key,
+                            Devices: device
+                        }
+                    })
+                    .value();
+
+                /*
+                for (let deviceGroup of groups) {
+                    // further divide groups that result to 8 columns and above
+                }
+                */
+
             });
         }
 
@@ -33,6 +53,24 @@ app.controller("DevicesController",
         loadDevicesByRoomId(params.id);
 
         startScreenService.refreshStartScreen();
+
+
+        scope.sizeOfGroup = function (size: number): string {
+
+            if (size > 24)
+                return "seven";
+            if (size > 20)
+                return "six";
+            if (size > 16)
+                return "five"
+            if (size > 12)
+                return "four";
+            if (size > 8)
+                return "three";
+            if (size > 4)
+                return "two"
+            return "";
+        }
         
     });
 
