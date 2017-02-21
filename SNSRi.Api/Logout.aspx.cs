0382170f8585692c7ea1ae6503e4753f10c6a449
jfaquinojr@gmail.com <jfaquinojr@gmail.com>
2017-02-21 22:18:38 +0800

    KALIKO IDENTITY

---
+++ SNSRi.Api/Logout.aspx.cs
@@ -0,0 +1,11 @@
+namespace SNSRi.Web {
+    using System;
+    using System.Web;
+
+    public partial class Logout : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            Context.GetOwinContext().Authentication.SignOut();
+            Response.Redirect("~/");
+        }
+    }
+}
